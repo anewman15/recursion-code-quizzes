da57b41ed36b833962fc2e2e756328a9e15b0fd1
Abdullah Numan <anewman15@hotmail.com>
2021-05-05 22:06:01 +0600

    Add cases for negative integers

--- factorial.rb
+++ factorial.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
 def factorial(num)
-  return num if num <= 1
+  return -1 if num.negative?
+  return 1 if num <= 1
 
   num * factorial(num - 1)
 end
 
+puts factorial(0)
+puts factorial(1)
+puts factorial(2)
 puts factorial(5)
+puts factorial(-5)
