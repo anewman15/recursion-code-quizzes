a0eae7b36ac90f1a3910bf829e9d05624f3d5c9e
Abdullah Numan <anewman15@hotmail.com>
2021-05-06 14:24:33 +0600

    User recursion to solve palindrome

--- palindrome.rb
+++ palindrome.rb
@@ -2,9 +2,15 @@
 
 def palindrome(string)
   text = string.gsub(/[^a-zA-Z0-9]/, '')
-  text == text.reverse
+  return true if text.length <= 1
+
+  return palindrome(string[1..-2]) if text[0] == text[-1]
+
+  false
 end
 
 puts palindrome('asdabcs dfs @dvsd')
 puts palindrome('asd !@%$@adsa')
 puts palindrome('race car')
+puts palindrome('a')
+puts palindrome('')
